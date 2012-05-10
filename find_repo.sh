2b1aac40453b10ef2c0e777fbd308efbebbfabd5
Brian McGillion <brian.mcgillion@intel.com>
2012-05-10 12:42:00 +0300

    Support multiple spec files

--- find_repo.sh
+++ find_repo.sh
@@ -3,7 +3,6 @@
 ROOT=`pwd`
 MANIFEST="$ROOT/manifest.manifest"
 SUCCESS="$ROOT/success.txt"
-FAILED_MULTIPLE_PACKAGES="$ROOT/failed_multiple_packages"
 FAILED_NO_SPEC="$ROOT/failed_no_spec"
 EMPTY_SPEC="$ROOT/empty_spec"
 
@@ -17,24 +16,22 @@ for dir in $(find $ROOT -name "\.git")
 do
     # the parent of the git directory is the base of the project
     cd $dir/..
+    count=0
     projectpath=$(relativepath)
     # only if there is a spec file for the project will we continue
-    if [ -e packaging/*.spec ] ; then
-	pathname=$(find packaging -name '*.spec')
+    path_names=$(find packaging -name '*.spec' 2> /dev/null)
+    for pathname in $path_names
+    do
 	name=$(basename $pathname ".spec")
 	if [ -s $pathname ] ; then
-	    if [ `grep -c "^%files\s*\(-f.*\)*$" $pathname` -eq 1 ] ; then
-		sed -i "s/^%files\s*\(-f.*\)*$/&\n\%manifest $name\.manifest/" $pathname
-		cp $MANIFEST ./"$name.manifest"
+	    sed -i "s/^%files.*$/&\n\%manifest $name\.manifest/" $pathname
+	    cp $MANIFEST ./"$name.manifest"
 	    # TODO ADD GIT COMMANDS
-		echo $projectpath >> $SUCCESS
-	    else
-		echo $projectpath >> $FAILED_MULTIPLE_PACKAGES
-	    fi
+	    echo $projectpath >> $SUCCESS
 	else
 	    echo $projectpath >> $EMPTY_SPEC
 	fi
-    else
-	echo $projectpath >> $FAILED_NO_SPEC 
-    fi
+	count=$(( count+1 ))
+    done
+    [ $count -eq 0 ] && echo $projectpath >> $FAILED_NO_SPEC 
 done
