6df89241161bc0aa0687091442c2b9be51f4a69d
Brian McGillion <brian.mcgillion@intel.com>
2012-05-10 11:20:01 +0300

    Only add manifest to files section if it is toplevel

--- find_repo.sh
+++ find_repo.sh
@@ -5,31 +5,36 @@ MANIFEST="$ROOT/manifest.manifest"
 SUCCESS="$ROOT/success.txt"
 FAILED_MULTIPLE_PACKAGES="$ROOT/failed_multiple_packages"
 FAILED_NO_SPEC="$ROOT/failed_no_spec"
+EMPTY_SPEC="$ROOT/empty_spec"
+
+function relativepath(){
+    python -c "import os.path; print os.path.relpath('$PWD', '$ROOT')";
+}
 
 # use the .git directory as a marker to find the relevant projects
 # in the directory hierarchy
 for dir in $(find $ROOT -name "\.git")
 do
-    pushd $ROOT
     # the parent of the git directory is the base of the project
     cd $dir/..
+    projectpath=$(relativepath)
     # only if there is a spec file for the project will we continue
     if [ -e packaging/*.spec ] ; then
 	pathname=$(find packaging -name '*.spec')
-	name=$(basename $pathname)
-	name=${name%.*}
-	if [ `grep -c "^\%files\s*$" $pathname` -eq 1 ] ; then
-	    sed -i "s/^\%files\s*$/%files\n\%manifest $name\.manifest/" $pathname
-	    cp $MANIFEST ./"$name.manifest"
+	name=$(basename $pathname ".spec")
+	if [ -s $pathname ] ; then
+	    if [ `grep -c "^%files\s*\(-f.*\)*$" $pathname` -eq 1 ] ; then
+		sed -i "s/^%files\s*\(-f.*\)*$/&\n\%manifest $name\.manifest/" $pathname
+		cp $MANIFEST ./"$name.manifest"
 	    # TODO ADD GIT COMMANDS
-	    echo "$name" >> $SUCCESS
+		echo $projectpath >> $SUCCESS
+	    else
+		echo $projectpath >> $FAILED_MULTIPLE_PACKAGES
+	    fi
 	else
-	    echo "$name" >> $FAILED_MULTIPLE_PACKAGES
+	    echo $projectpath >> $EMPTY_SPEC
 	fi
     else
-	echo `pwd` >> $FAILED_NO_SPEC 
+	echo $projectpath >> $FAILED_NO_SPEC 
     fi
-    # go back to the base directory
-    popd
 done
-
