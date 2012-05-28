7e0cae46152e5bfe713f69f1c9941cbb2d5a8ff5
Brian McGillion <brian.mcgillion@intel.com>
2012-05-28 18:49:13 +0300

    Move manifest to the packaging dir

--- find_repo.sh
+++ find_repo.sh
@@ -2,9 +2,10 @@
 
 ROOT=`pwd`
 MANIFEST="$ROOT/manifest.manifest"
-SUCCESS="$ROOT/success.txt"
-FAILED_NO_SPEC="$ROOT/failed_no_spec"
-EMPTY_SPEC="$ROOT/empty_spec"
+SUCCESS="$ROOT/success.result"
+FAILED_NO_SPEC="$ROOT/failed_no_spec.result"
+EMPTY_SPEC="$ROOT/empty_spec.result"
+ALREADY_DONE="$ROOT/already_done.result"
 
 function relativepath(){
     python -c "import os.path; print os.path.relpath('$PWD', '$ROOT')";
@@ -23,10 +24,25 @@ do
     for pathname in $path_names
     do
 	name=$(basename $pathname ".spec")
+	manifest_dst="packaging/$name.manifest"
+	# if a manifest exists assume we have already done the work on this package.
+	if [ -e $manifest_dst ] ; then
+	    echo $projectpath >> $ALREADY_DONE
+	    continue
+	fi
+	# make sure the spec file is not empty
 	if [ -s $pathname ] ; then
-	    sed -i "s/^%files.*$/&\n\%manifest $name\.manifest/" $pathname
-	    cp $MANIFEST ./"$name.manifest"
-	    git add "$name.manifest" "$pathname"
+	    # The dirtiest hack that I could come up with without writing real code :)
+	    tac $pathname | sed "0,/^Source/ { /^Source/i \
+Source1001: $manifest_dst 
+}" | tac > tmp.spec ; mv tmp.spec $pathname
+	    
+	    sed -i '/^%build/ a \
+cp %{SOURCE1001} .' $pathname
+	    sed -i "/^%files/ a \
+%manifest $name\.manifest" $pathname
+	    cp $MANIFEST ./"$manifest_dst"
+	    git add "$manifest_dst" "$pathname"
 	    git commit -s -m "Add default Smack manifest for $name.spec"
 	    echo $projectpath >> $SUCCESS
 	else
